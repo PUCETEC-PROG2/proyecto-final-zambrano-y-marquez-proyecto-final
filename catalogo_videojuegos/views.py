d5f5104604373298458266167e160235ef8016d7
Josue Zambrano <zambrano.josue.99@gmail.com>
2024-08-06 21:06:34 -0500

    configurar urls app y primera vista

--- catalogo_videojuegos/views.py
+++ catalogo_videojuegos/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.template import loader
+from django.db import connection
 
-# Create your views here.
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
