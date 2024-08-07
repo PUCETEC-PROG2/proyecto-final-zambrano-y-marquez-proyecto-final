34c67c4208e7b250340c78104de136d5e357b66a
Josue Zambrano <zambrano.josue.99@gmail.com>
2024-08-06 20:59:27 -0500

    configurar base de datos, urls, e instalar requisitos

--- proyecto_final/settings.py
+++ proyecto_final/settings.py
@@ -76,9 +76,18 @@ WSGI_APPLICATION = 'proyecto_final.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trabajo_final_grupo5',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+    
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
